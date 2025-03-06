4d488b086dfa8773abc3f8c7c87ecaf990061b23
tangjiaqiang <tangjiaqiang@heytea.com>
2025-03-06 13:48:17 +0800

    feat: update biome output

--- packages/biome-config-utils/rslib.config.ts
+++ packages/biome-config-utils/rslib.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from '@rslib/core';
+import { defineConfig } from "@rslib/core";
 
 export default defineConfig({
-  source:{
-    entry:{
-      index:"src/index.ts"
-    }
+  source: {
+    entry: {
+      index: "src/index.ts",
+    },
   },
   lib: [
     {
-      format: 'esm',
-      syntax: 'es2021',
+      format: "esm",
+      syntax: "es2021",
       dts: false,
-      bundle: false
+      bundle: false,
     },
   ],
+  output: {
+    copy: {
+      patterns:[
+        "./biome.json",
+      ]
+    },
+  },
 });
