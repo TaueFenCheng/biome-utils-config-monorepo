14d7c078ffa09165c5aead49adab54691b38c040
tangjiaqiang <tangjiaqiang@heytea.com>
2025-03-07 10:46:45 +0800

    feat: update config utils

--- packages/utils/rslib.config.ts
+++ packages/utils/rslib.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from '@rslib/core'
 
 export default defineConfig({
+  source:{
+    entry:{
+      index:"./src/index.ts"
+    }
+  },
   lib: [
     {
       format: 'esm',
       syntax: 'es2021',
-      dts: true,
+      dts: false,
+      autoExtension:true,
+      output:{
+        filename:{
+          js:'[name].mjs'
+        }
+      }
     },
     {
       format: 'cjs',
       syntax: 'es2021',
+      autoExtension:true
     },
   ],
 })
---
+++ packages/utils/src/render.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from 'react';
+import { RootType, render as copyRender } from './ReactDomRender';
+
+export class ReactDOMRender<P = {}> {
+  root: RootType | undefined;
+
+  app: FunctionComponent<P>;
+
+  container: Element | DocumentFragment;
+
+  constructor(
+    app: FunctionComponent<P>,
+    container: Element | DocumentFragment,
+  ) {
+    this.app = app;
+    this.container = container;
+  }
+
+  render = (props: any) => {
+    const CustomApp = this.app;
+    if (this.root) {
+      this.root.render(<CustomApp {...props} />);
+    } else {
+      this.root = copyRender(<CustomApp {...props} />, this.container);
+    }
+  };
+
+  unmount = () => {
+    this.root?._unmount();
+    this.root = undefined;
+  };
+}
