2adbba060268ef63d3a3f0dd045d9e953a2230a9
tjq <tangtaue0826@163.com>
2025-08-03 00:58:35 +0800

    chore: lint

--- packages/utils/src/is.ts
+++ packages/utils/src/is.ts
@@ -1,74 +1,73 @@
-
-const opt = Object.prototype.toString;
+const opt = Object.prototype.toString
 
 export function isArray(obj: any): obj is any[] {
-  return opt.call(obj) === '[object Array]';
+  return opt.call(obj) === '[object Array]'
 }
 
 export function isObject(obj: any): obj is { [key: string]: any } {
-  return opt.call(obj) === '[object Object]';
+  return opt.call(obj) === '[object Object]'
 }
 
 export function isString(obj: any): obj is string {
-  return opt.call(obj) === '[object String]';
+  return opt.call(obj) === '[object String]'
 }
 
 export function isNumber(obj: any): obj is number {
-  return opt.call(obj) === '[object Number]' && obj === obj; // eslint-disable-line
+  return opt.call(obj) === '[object Number]' && obj === obj // eslint-disable-line
 }
 
 export function isRegExp(obj: any) {
-  return opt.call(obj) === '[object RegExp]';
+  return opt.call(obj) === '[object RegExp]'
 }
 
 export function isFile(obj: any): obj is File {
-  return opt.call(obj) === '[object File]';
+  return opt.call(obj) === '[object File]'
 }
 
 export function isBlob(obj: any): obj is Blob {
-  return opt.call(obj) === '[object Blob]';
+  return opt.call(obj) === '[object Blob]'
 }
 
 function isHex(color) {
-  return /^#[a-fA-F0-9]{3}$|#[a-fA-F0-9]{6}$/.test(color);
+  return /^#[a-fA-F0-9]{3}$|#[a-fA-F0-9]{6}$/.test(color)
 }
 
 function isRgb(color) {
-  return /^rgb\((\s*\d+\s*,?){3}\)$/.test(color);
+  return /^rgb\((\s*\d+\s*,?){3}\)$/.test(color)
 }
 
 function isRgba(color) {
-  return /^rgba\((\s*\d+\s*,\s*){3}\s*\d(\.\d+)?\s*\)$/.test(color);
+  return /^rgba\((\s*\d+\s*,\s*){3}\s*\d(\.\d+)?\s*\)$/.test(color)
 }
 export function isColor(color: any): boolean {
-  return isHex(color) || isRgb(color) || isRgba(color);
+  return isHex(color) || isRgb(color) || isRgba(color)
 }
 export function isUndefined(obj: any): obj is undefined {
-  return obj === undefined;
+  return obj === undefined
 }
 
 export function isNull(obj: any): obj is null {
-  return obj === null;
+  return obj === null
 }
 
 export function isNullOrUndefined(obj: any): boolean {
-  return obj === null || obj === undefined;
+  return obj === null || obj === undefined
 }
 
 export function isFunction(obj: any): obj is (...args: any[]) => any {
-  return typeof obj === 'function';
+  return typeof obj === 'function'
 }
 
 export function isEmptyObject(obj: any): boolean {
-  return isObject(obj) && Object.keys(obj).length === 0;
+  return isObject(obj) && Object.keys(obj).length === 0
 }
 
 export function isExist(obj: any): boolean {
-  return obj || obj === 0;
+  return obj || obj === 0
 }
 
 export function isWindow(el: any): el is Window {
-  return el === window;
+  return el === window
 }
 
 // export function isDayjs(time): time is Dayjs {
@@ -86,6 +85,6 @@ export function isWindow(el: any): el is Window {
 //   );
 // }
 
-export function isBoolean(value: any): value is Boolean {
-  return typeof value === 'boolean';
+export function isBoolean(value: any): value is boolean {
+  return typeof value === 'boolean'
 }
--- packages/utils/src/render.tsx
+++ packages/utils/src/render.tsx
@@ -1,32 +1,32 @@
-import React, { FunctionComponent } from 'react';
-import { RootType, render as copyRender } from './ReactDomRender';
+import React, { type FunctionComponent } from 'react'
+import { type RootType, render as copyRender } from './ReactDomRender'
 
 export class ReactDOMRender<P = {}> {
-  root: RootType | undefined;
+  root: RootType | undefined
 
-  app: FunctionComponent<P>;
+  app: FunctionComponent<P>
 
-  container: Element | DocumentFragment;
+  container: Element | DocumentFragment
 
   constructor(
     app: FunctionComponent<P>,
     container: Element | DocumentFragment,
   ) {
-    this.app = app;
-    this.container = container;
+    this.app = app
+    this.container = container
   }
 
   render = (props: any) => {
-    const CustomApp = this.app;
+    const CustomApp = this.app
     if (this.root) {
-      this.root.render(<CustomApp {...props} />);
+      this.root.render(<CustomApp {...props} />)
     } else {
-      this.root = copyRender(<CustomApp {...props} />, this.container);
+      this.root = copyRender(<CustomApp {...props} />, this.container)
     }
-  };
+  }
 
   unmount = () => {
-    this.root?._unmount();
-    this.root = undefined;
-  };
+    this.root?._unmount()
+    this.root = undefined
+  }
 }
